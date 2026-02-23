1c859ddd0889264c50b34808c4f96ee8d2d456ff
Michael Gestinger <gestinger.michael@gmail.com>
2026-02-23 12:06:59 +0100

    add multiple rss sources

--- fetch.py
+++ fetch.py
@@ -5,42 +5,59 @@ from supabase import create_client
 
 print("🚀 Spouštím stahování zpráv...")
 
-SUPABASE_URL = os.getenv("SUPABASE_URL")
-SUPABASE_KEY = os.getenv("SUPABASE_KEY")
+SUPABASE_URL = os.getenv("https://ncxcpeeocknrotbzmfde.supabase.co")
+SUPABASE_KEY = os.getenv("sb_publishable_L1AIP3X0SS5_-S5SbX0M7Q_KMWjfCLj")
 
 supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
 
-RSS_URL = "https://ct24.ceskatelevize.cz/rss/hlavni-zpravy"
+RSS_SOURCES = {
+    "ct24": "https://ct24.ceskatelevize.cz/rss/hlavni-zpravy",
+    "irozhlas": "https://www.irozhlas.cz/rss/irozhlas",
+    "novinky": "https://www.novinky.cz/rss"
+}
 
-feed = feedparser.parse(RSS_URL)
+# Zakázaná slova
+BANNED_WORDS = ["vražda", "vraždy", "vraždou", "vraždami", "vraždě", "vražd", "Ukrajina", "Ukrajině", "Ukrajinou", "Ukrajiny", "ukrajinci"]
 
-print("Nalezeno článků:", len(feed.entries))
+for source_name, rss_url in RSS_SOURCES.items():
+    print(f"\n🔎 Stahuji ze zdroje: {source_name}")
 
-for entry in feed.entries:
-    title = entry.title
-    url = entry.link
-    content = entry.summary if "summary" in entry else ""
+    feed = feedparser.parse(rss_url)
 
-    hash_value = hashlib.sha256((title + content).encode("utf-8")).hexdigest()
+    print("Nalezeno článků:", len(feed.entries))
 
-    existing = supabase.table("articles") \
-        .select("id") \
-        .or_(f"url.eq.{url},hash.eq.{hash_value}") \
-        .execute()
+    for entry in feed.entries:
+        title = entry.title
+        url = entry.link
+        content = entry.summary if "summary" in entry else ""
 
-    if existing.data:
-        print("⛔ Už existuje:", title)
-        continue
+        text_to_check = (title + " " + content).lower()
 
-    data = {
-        "title": title,
-        "url": url,
-        "content": content,
-        "source": "ct24",
-        "hash": hash_value
-    }
+        # ❌ blacklist
+        if any(banned in text_to_check for banned in BANNED_WORDS):
+            print("🚫 Zakázané slovo:", title)
+            continue
 
-    supabase.table("articles").insert(data).execute()
-    print("✅ Uloženo:", title)
+        hash_value = hashlib.sha256((title + content).encode("utf-8")).hexdigest()
+
+        existing = supabase.table("articles") \
+            .select("id") \
+            .or_(f"url.eq.{url},hash.eq.{hash_value}") \
+            .execute()
+
+        if existing.data:
+            print("⛔ Už existuje:", title)
+            continue
+
+        data = {
+            "title": title,
+            "url": url,
+            "content": content,
+            "source": source_name,
+            "hash": hash_value
+        }
+
+        supabase.table("articles").insert(data).execute()
+        print("✅ Uloženo:", title)
 
 print("🎯 Hotovo")
